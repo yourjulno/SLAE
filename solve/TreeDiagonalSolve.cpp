4ce7206a75a9b1a824abca3a3a2e62d6641a1210
yourjulno <diugaeva.iud@phystech.edu>
2023-02-06 23:11:57 +0300

    change some shhhhhit (it works!!!!!)

--- solve/TreeDiagonalSolve.cpp
+++ solve/TreeDiagonalSolve.cpp
@@ -7,39 +7,42 @@
 std::vector<double> SolveOfTridiagonal::solution_data;
 std::vector<double> SolveOfTridiagonal::find_x(std::vector<double> &p, std::vector<double> &q) {
 //    solution_data.resize(p.size());
-    for (int i = 0; i != p.size(); i++){
-        double x_prev = p[i] * solution_data.back() + q[i];
-        solution_data.push_back(x_prev);
+
+    for (int i = p.size() - 2; i >= 0; i--){
+        solution_data[i] = p[i] * solution_data[i + 1]+ q[i];
+
     }
     return solution_data;
 };
 
 std::vector<double> SolveOfTridiagonal::find_q_p(const ThirdDiagonalMatrix &start_matrix,
                                                  const std::vector<double> &free_) {
-//    solution_data.resize(start_matrix.rows());
-    std::vector<double> p_;
-    std::vector<double> q_;
+
+    unsigned int n = free_.size();
+    solution_data.resize(n);
+    std::vector<double> p_(n);
+    std::vector<double> q_(n);
 
 
-    const double a_0 = start_matrix.operator()(1, 0);
+
     const double b_0 = start_matrix.operator()(0, 0);
     const double c_0 = start_matrix.operator()(0, 1);
 
-    const double p_1 = - (c_0 / b_0);
-    const double q_1 = free_[0] / a_0;
-    p_.push_back(p_1);
-    q_.push_back(q_1);
+    const double p_1 = - c_0 / b_0;
+    const double q_1 = free_[0] / b_0;
+    p_[0] = p_1;
+    q_[0] = q_1;
 
-    for (int i = 1; i != start_matrix.rows(); i++){
-        double p = - start_matrix.operator()(i, 2)/(start_matrix.operator()(i, 0)*p_.back() + start_matrix.operator()(i, 1));
-        p_.push_back(p);
+    for (int i = 1; i < start_matrix.rows() - 1; i++){
+        double p = - start_matrix(i, 2)/(start_matrix(i, 0)*p_[i - 1] + start_matrix(i, 1));
+        p_[i] = p;
 
-        double q = free_[i] - start_matrix.operator()(i, 0) / (start_matrix.operator()(i, 0) * p_.back() + start_matrix.operator()(i, 1));
-        q_.push_back(q);
+        double q = (free_[i] - start_matrix(i, 0) * q_[i - 1]) / (start_matrix(i, 0) * p_[i - 1] + start_matrix(i, 1));
+        q_[i] = q;
     }
 
-    double x_n = (free_.back() - (start_matrix.operator()(start_matrix.rows() - 1, 1)*q_.back())) /(start_matrix.operator()(start_matrix.rows() - 1, 1)*p_.back() +start_matrix.operator()(start_matrix.rows() - 1, 2));
-    solution_data.push_back(x_n);
+    double x_n = (free_.back() - (start_matrix(start_matrix.rows() - 1, 1)*q_.back()))/(start_matrix(start_matrix.rows() - 1, 1)*p_.back() + start_matrix(start_matrix.rows() - 1, 2));
+    solution_data[n - 1] = x_n;
     std::vector<double> solution = find_x(p_,q_);
     return solution;
 }
