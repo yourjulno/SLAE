9ffe514eb90c567946ef034b92cd6f37178e6c16
yourjulno <diugaeva.iud@phystech.edu>
2023-02-06 12:07:30 +0300

    solve of Tridiagonal matrix

---
+++ solve/TreeDiagonalSolve.h
@@ -0,0 +1,18 @@
+//
+// Created by julia on 05.02.23.
+//
+#include <vector>
+#include "../ThreeDiagonalMatrix/three_diagonal.h"
+#ifndef SLAE_TREEDIAGONALSOLVE_H
+#define SLAE_TREEDIAGONALSOLVE_H
+class SolveOfTridiagonal {
+    static std::vector<double> solution_data;
+public:
+
+    static std::vector<double> find_q_p(const ThirdDiagonalMatrix &start_matrix,
+                                        const std::vector<double> &free_);
+
+
+    static std::vector<double> find_x(std::vector<double>& p, std::vector<double>& q);
+};
+#endif //SLAE_TREEDIAGONALSOLVE_H
