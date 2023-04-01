763f76b5732c21b57e4a8b7e634c14bc98c2c603
yourjulno <diugaeva.iud@phystech.edu>
2023-04-01 13:37:02 +0300

    ускорение для метода ГЗ

--- Tests/test_GZ.cpp
+++ Tests/test_GZ.cpp
@@ -5,14 +5,32 @@
 #include <gtest/gtest.h>
 #include "../src/sparse/sparse.hpp"
 #include "../src/solve/GZ.hpp"
-int main(){
-    std::set<Tri<double>> example = {{0,0,100},{0,1,0.2},
-                                     {1,0,0.2},{1,1,100}};
-    std::vector<double> free = {1000, 999};
-    std::vector<double> x = {0,0};
+#include "../src/solve/MPI.hpp"
+
+TEST(iteration, GZ_fast_){
+    std::set<Tri<double>> example2 = {{0,0,10},{0,1,1},
+                                      {1,0,1},{1,1,7},{1,2, 0.1}, {2,2,1} };
+    std::vector<double> free2 = {20, 30, 1};
+    std::vector<double> x = {0,0, 0};
 
 
-    CSR<double> first(example, 2, 2);
-    auto res = GZ(first, x,free, pow(10,-12));
+    CSR<double> first2(example2, 3, 3);
+    auto res = GZFast(first2, x, free2, 1e-5, 0.233);
+    std::cout << res.second;
 
+}
+
+TEST(iteration_GZ_fast__Test, GZFast_){
+    std::set<Tri<double>> example2 = {{0,0,10},{0,1,1},
+                                      {1,0,1},{1,1,7},{1,2, 0.1}, {2,2,1} };
+    std::vector<double> free2 = {20, 30, 1};
+    std::vector<double> x = {0,0, 0};
+
+
+    CSR<double> first2(example2, 3, 3);
+    double lambda_max = max_lambda(first2);
+    double precision = 1e-12;
+    auto res_from_GZ_fast = GZFast(first2, x, free2, precision, lambda_max);
+    auto res_from_GZ_slow = GZ(first2, x, free2, precision);
+    ASSERT_GE(res_from_GZ_fast.second, res_from_GZ_slow.second);
 }
--- src/solve/GZ.hpp
+++ src/solve/GZ.hpp
@@ -39,4 +39,78 @@ std::pair<std::vector<T>,int> GZ(const CSR<T> &A, const std::vector<T> &x, const
     std::pair<std::vector<double>, int> k = {solve, count};
     return k;
 }
+
+template <class T>
+std::vector<T> iter_for_GZ(const CSR<T> &A, const std::vector<T> &x, const std::vector<T> &b){
+    std::vector<T> x_ = x;
+    T res;
+    //0.5
+    for (int i = 0; i < A.getCol(); i++){
+        res = 0;
+        T d;
+        for (int j = A.getMatrRows(i); j < A.getMatrRows(i + 1); j++) {
+
+            if (i != A.getColns(j)) {
+                res = res + A.getValues(j)*x_[A.getColns(j)];
+            }
+            else
+                d = A(i, i);
+        }
+        x_[i] = (b[i] - res)/d;
+    }
+    //1
+    for (int i = A.getCol() - 1; i >= 0; i--){
+        res = 0;
+        T d;
+        for (int j = A.getMatrRows(i); j < A.getMatrRows(i + 1); j++) {
+
+            if (i != A.getColns(j)) {
+                res = res + A.getValues(j)*x_[A.getColns(j)];
+            }
+            else
+                d = A(i, i);
+        }
+        x_[i] = (b[i] - res)/d;
+    }
+    return x_;
+}
+
+template <class T>
+std::pair<std::vector<T>, std::size_t> GZFast(const CSR<T> &A, const std::vector<T> &x,
+                                      const std::vector<T> &b, T tolerance, T ro){
+
+
+    std::vector<T> y0 = x;
+    std::vector<T> y_1 = iter_for_GZ(A, y0, b);
+
+    std::vector<T> y(x.size());
+    std::vector<T> solve = A * y_1 - b;
+
+    T nu0 = 1;
+    T nu1 = 1 / ro;
+    int count = 0;
+    T res;
+    T temp;
+
+    while (lenght(solve) > tolerance){
+
+        y_1 = iter_for_GZ(A, y_1, b);
+        y = 2 * nu1 / ro * y_1 - nu0 * y0;
+        nu0 = 2 / ro * nu1 - nu0;
+        y = y / nu0;
+
+        y0 = y_1;
+        y_1 = y;
+
+        std::swap(nu0, nu1);
+
+
+        solve = A * y - b;
+        count++;
+    }
+
+    std::pair<std::vector<double>, int> k = {solve, count};
+    return k;
+}
+
 #endif //SLAE_GZ_HPP
